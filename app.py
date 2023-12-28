e070550e82528448604a5bccb88abe66e5dc4f06
phyoehtetkyaw <phyoehtetkyaw@brain-inter.com>
2023-12-28 15:27:45 +0630

    add wainain-dev

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import  Flask, render_template, request, redirect, jsonify
+from flask import  Flask, render_template, request, redirect
 import sqlite3, datetime
 
 app = Flask(__name__)
